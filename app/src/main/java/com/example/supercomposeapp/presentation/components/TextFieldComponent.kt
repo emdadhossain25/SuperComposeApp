f276ce07831f2ab23e627ef5c43b73c68f826cd0
emdad <emdad@ergo-ventures.com>
2023-06-22 20:32:51 +0600

    TextFieldComponent design done

---
+++ app/src/main/java/com/example/supercomposeapp/presentation/components/TextFieldComponent.kt
@@ -0,0 +1,133 @@
+package com.example.supercomposeapp.presentation.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.supercomposeapp.ui.theme.Bariol
+import com.example.supercomposeapp.ui.theme.Typography
+import com.example.supercomposeapp.ui.theme.textColorLightGray
+import com.example.supercomposeapp.ui.theme.white
+
+@Composable
+fun TextFieldComponent(
+
+    description: String,
+    textValue: String,
+    keyboardType: KeyboardType = KeyboardType.Ascii,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onValueChanged: (String) -> Unit,
+    trailingIcon: ImageVector? = null,
+    onTrailingIconClick: (() -> Unit)?, // lambda function does not take anything, return anything , also optional
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle,
+    textSize: TextUnit,
+    textColor: Color,
+    cursorColor: Color,
+    placeHolderText: String,
+    placeHolderTextColor: Color,
+    enabled: Boolean,
+    trailingIconClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        TextField(
+            modifier = modifier
+                .fillMaxWidth()
+                .border(
+                    0.5.dp,
+                    textColor,
+                    RoundedCornerShape(25.dp)
+                )
+                .height(50.dp)
+                .shadow(
+                    3.dp,
+                    RoundedCornerShape(25.dp)
+                ),
+            value = textValue,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = textColor,
+                disabledTextColor = textColor,
+                placeholderColor = placeHolderTextColor,
+                backgroundColor = white,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            onValueChange = onValueChanged,
+            shape = RoundedCornerShape(25.dp),
+            singleLine = true,
+
+
+            trailingIcon = {
+                if (trailingIcon != null) {
+                    Icon(imageVector = trailingIcon, contentDescription = null, tint = cursorColor,
+                        modifier = Modifier
+                            .height(20.dp)
+                            .width(20.dp)
+                            .clickable {
+                                if (onTrailingIconClick != null) onTrailingIconClick()
+                            })
+                }
+            },
+            placeholder = {
+                Text(
+                    text = placeHolderText,
+                    fontFamily = Bariol,
+                    fontSize = textSize,
+                    style = textStyle,
+                    textAlign = TextAlign.Left
+
+                )
+
+            },
+            textStyle = textStyle,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            visualTransformation = visualTransformation
+        )
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TextFieldComponentPreview() {
+    TextFieldComponent(
+        description = "Email Address",
+        modifier = Modifier.fillMaxWidth(),
+        placeHolderText = "emdad@ergo-ventures.com",
+        textValue = "",
+        textColor = textColorLightGray,
+        trailingIcon = Icons.Filled.Email,
+        cursorColor = textColorLightGray,
+        onValueChanged = {},
+        onTrailingIconClick = {},
+        textStyle = Typography.h2,
+        enabled = true,
+        trailingIconClick = {},
+        textSize = 18.sp,
+        placeHolderTextColor = textColorLightGray
+
+    )
+}
--- app/src/main/java/com/example/supercomposeapp/presentation/screen/UserInformationScreen.kt
+++ app/src/main/java/com/example/supercomposeapp/presentation/screen/UserInformationScreen.kt
@@ -3,17 +3,23 @@ package com.example.supercomposeapp.presentation.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.supercomposeapp.R
 import com.example.supercomposeapp.presentation.components.ButtonComponent
 import com.example.supercomposeapp.presentation.components.TextComponent
-import com.example.supercomposeapp.ui.theme.*
+import com.example.supercomposeapp.presentation.components.TextFieldComponent
+import com.example.supercomposeapp.ui.theme.Typography
+import com.example.supercomposeapp.ui.theme.darkMagneta
+import com.example.supercomposeapp.ui.theme.textColorLightGray
+import com.example.supercomposeapp.ui.theme.white
 
 @Composable
 fun UserInformationScreen() {
@@ -29,6 +35,7 @@ fun UserInformationScreen() {
             fontSize = 35.sp,
             textStyle = Typography.h1,
         )
+
         TextComponent(
             modifier = Modifier
                 .fillMaxWidth()
@@ -38,6 +45,27 @@ fun UserInformationScreen() {
             fontSize = 17.sp,
             textStyle = Typography.h2
         )
+        TextFieldComponent(
+            description = "Email Address",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            placeHolderText = "emdad@ergo-ventures.com",
+            textValue = "",
+            textColor = textColorLightGray,
+            trailingIcon = Icons.Filled.Email,
+            cursorColor = textColorLightGray,
+            onValueChanged = {},
+            onTrailingIconClick = {},
+            textStyle = Typography.h2,
+            enabled = true,
+            trailingIconClick = {},
+            textSize = 17.sp,
+
+            placeHolderTextColor = textColorLightGray
+
+        )
+
 
         ButtonComponent(
             modifier = Modifier
@@ -48,11 +76,12 @@ fun UserInformationScreen() {
             contentColor = white,
             onButtonClick = { /*TODO*/ },
             isLoading = false,
-            borderColor = transparent,
+            borderColor = Color.Transparent,
             buttonHeight = 40.dp,
             fontSize = 20.sp,
             style = Typography.h1
         )
+
     }
 }
 
